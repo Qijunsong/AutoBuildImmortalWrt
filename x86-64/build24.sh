31a1c842e0273c2a7facab67ede1086d5cc9701e
Qijunsong <102445267+Qijunsong@users.noreply.github.com>
2025-12-17 13:30:58 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -79,7 +79,8 @@ PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
 PACKAGES="$PACKAGES luci-app-unishare"
 PACKAGES="$PACKAGES iptables-nft"
-
+PACKAGES="$PACKAGES kmod-fuse"
+PACKAGES="$PACKAGES fuse-utils"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
